707cc30cb1046d73baaa6d90eef8d70ecfdbfd23
Karan Nagpal <karannagpal96@gmail.com>
2025-01-27 21:01:30 +0530

    Fix: layouts of Navbar and Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Container from "@/components/Container";
 
 function Footer() {
   return (
-    <footer className="border-dashed border-neutral-500 border p-2 rounded">
-      This is footer
+    <footer className="bg-neutral-100 py-2">
+      <Container>
+        <div className="flex justify-between">
+          <div className="font-bold">Blogging platform core</div>
+          <div className="flex gap-2">
+            <div>About</div>
+            <div>Blogs</div>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 }
